e9f0412d53997a8d2baf6b7baf2a130c4540fb8e
Michael Wheeler <echopeakdev@gmail.com>
2025-10-19 10:33:56 -0400

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/uml-board/" : "/",
+  base: mode === "production" ? "/VectorUML/" : "/",
   server: {
     host: "::",
     port: 8080,
